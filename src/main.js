6b7bc0756e0779d488c2a2bf5a00335009bc5087
qinlicang <18303033133@163.com>
2019-09-05 15:41:39 +0800

    add element ui

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import VueBus from '@/config/vue-bus-plugIn';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App';
 import router from './router';
 import stores from './store/index';
 
 Vue.use(VueBus);
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
